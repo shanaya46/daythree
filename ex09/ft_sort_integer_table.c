5136dd71888260746a1e98766ddca87b107a03ef
Shanaya Jain <shanayajain2001@gmail.com>
2018-07-15 22:16:28 -0700

    exercise 9

--- ex09/ft_sort_integer_table.c
+++ ex09/ft_sort_integer_table.c
@@ -4,24 +4,30 @@
 
 void ft_sort_integer_table (int *tab, int size) {
 int num = 0;
-for (int b = 1; b <= 2; b++){
-for (int i = 0; i < (size); i++) {
-    if (tab[i] > tab [i + 1]) {
-    num = tab[i];
-    tab[i] = tab[i + 1];
-    tab[i + 1] = num;
+for (int i = size - 1; i >= 0; i--) {
+    for (int j = 0; j < i; j++) {
+        if (tab[j] > tab [j + 1]) {
+            num = tab[j];
+            tab[j] = tab[j + 1];
+            tab[j + 1] = num;
+        }
     }
-}
+    printf("i = %d: ", i);
+    for (int a = 0; a < size; a++){
+        printf(" %d ", tab[a]);
+    }
+    printf("\n");
+
 }
 
 for (int a = 0; a < size; a++){
     printf("%d ", tab[a]);
 }
-   printf("\n");
+printf("\n");
 
 }
 
 int main () {
-int tab[5] = {3, 7, 4, 10, 6};
-ft_sort_integer_table (tab, 5);
+int tab[10] = {37, 17, 90, 10, 6, 18, 3, 1, 20, 34};
+ft_sort_integer_table (tab, 10);
 }
